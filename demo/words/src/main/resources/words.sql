db28c49396592841d2bbd2f3acb8828045541b08
aiordache <anca.iordache@docker.com>
2020-05-15 13:53:42 +0200

    sentences app

---
+++ demo/words/src/main/resources/words.sql
@@ -0,0 +1,52 @@
+
+INSERT INTO nouns(word) VALUES
+  ('cloud'),
+  ('elephant'),
+  ('gø language'),
+  ('laptøp'),
+  ('cøntainer'),
+  ('micrø-service'),
+  ('turtle'),
+  ('whale'),
+  ('gøpher'),
+  ('møby døck'),
+  ('server'),
+  ('bicycle'),
+  ('viking'),
+  ('mermaid'),
+  ('fjørd'),
+  ('legø'),
+  ('flødebolle'),
+  ('smørrebrød');
+
+INSERT INTO verbs(word) VALUES
+  ('will drink'),
+  ('smashes'),
+  ('smøkes'),
+  ('eats'),
+  ('walks tøwards'),
+  ('løves'),
+  ('helps'),
+  ('pushes'),
+  ('debugs'),
+  ('invites'),
+  ('hides'),
+  ('will ship');
+
+INSERT INTO adjectives(word) VALUES
+  ('the exquisite'),
+  ('a pink'),
+  ('the røtten'),
+  ('a red'),
+  ('the serverless'),
+  ('a brøken'),
+  ('a shiny'),
+  ('the pretty'),
+  ('the impressive'),
+  ('an awesøme'),
+  ('the famøus'),
+  ('a gigantic'),
+  ('the gløriøus'),
+  ('the nørdic'),
+  ('the welcøming'),
+  ('the deliciøus');
