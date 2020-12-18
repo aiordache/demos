ed8ae814c0af9300b8791f093bcc70eaa0f6e439
aiordache <anca.iordache@docker.com>
2020-12-18 13:08:44 +0100

    Add sample for demo-ing GPU support in Docker Compose
    Signed-off-by: aiordache <anca.iordache@docker.com>

---
+++ gpublog-demo/README.md
@@ -0,0 +1,8 @@
+### DEMO GPU support in Docker Compose
+
+`model_generator.py` contains sample code from the Tensorflow docs at https://www.tensorflow.org/tutorials/text/nmt_with_attention
+
+When deploying on a host without an Nvidia GPU device, remove the `deploy` section from the Compose file, otherwise `docker-compose` will request the allocation of a GPU which will fail if the host does not have one.
+
+If no GPU is detected, the tensorflow framework will default to CPU. Training will take very long in this case.
+
---
+++ gpublog-demo/backend/server.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from flask import Flask, request, make_response, jsonify
+from model_generator import TranslatorModel
+
+server = Flask(__name__)
+
+model = None
+
+@server.route('/reload', methods = ['GET'])
+def reload():
+  global model
+  lang = request.args.get('lang', default = None, type = str)
+  if not lang:
+    return make_response('', 200)
+  model = TranslatorModel(lang)
+  return make_response('', 200)
+    
+
+@server.route('/', methods = ['GET', 'POST'])
+def translate():
+  global model
+  text = request.args.get('text', default = None, type = str) or request.form.get('text') 
+  if text == '':
+    return make_response('', 200)
+  if not model:
+    return make_response('Training in progress...', 200)
+
+  t = model.translate(text)
+  return make_response(t, 200)
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', debug=False)
